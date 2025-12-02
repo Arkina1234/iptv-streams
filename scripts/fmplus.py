1649a3d08c89507166219c7321dc489244290272
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-12-02 07:54:57 -0400

    Update fmplus.py

--- scripts/fmplus.py
+++ scripts/fmplus.py
@@ -4,124 +4,135 @@ from datetime import datetime
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-# Fetch data from the API
+# Fetch data from API
 url = "https://epg.unreel.me/v2/sites/fmplus/live-channels/public/081f73704b56aaceb6b459804761ec54"
 response = requests.get(url)
 data = response.json()
 
-# Initialize lists for storing output
+# Initialize lists for M3U and XML
 m3u_lines = []
 channels = []
-programmes = []
+programs = []
 
-# Create XML root
+# M3U header
+m3u_lines.append('#EXTM3U url-tvg="https://arkina1234.github.io/iptv-streams/epg/fmplus.xml"')
+
+# Process each channel
+for channel in data:
+    # Extract channel info
+    channel_id = channel.get('_id', '')
+    name = channel.get('name', '')
+    
+    # Get thumbnails
+    thumbnails = channel.get('thumbnails', {})
+    thumbnail_url = thumbnails.get('light', '') if thumbnails else ''
+    
+    # Get site options
+    site_options = channel.get('siteOptions', {})
+    channel_number = site_options.get('channelNumber', '')
+    
+    # Get URL and encode brackets
+    stream_url = channel.get('url', '')
+    if stream_url:
+        stream_url = stream_url.replace('[', '%5B').replace(']', '%5D')
+    
+    # Get EPG data
+    epg = channel.get('epg', {})
+    epg_entries = epg.get('entries', [])
+    
+    # Process first EPG entry if exists
+    if epg_entries:
+        epg_entry = epg_entries[0]
+        epg_description = epg_entry.get('description', '')
+        epg_start = epg_entry.get('start', '')
+        epg_stop = epg_entry.get('stop', '')
+        epg_title = epg_entry.get('title', '')
+        
+        # Format start and stop times
+        try:
+            start_time = datetime.strptime(epg_start, '%Y-%m-%dT%H:%M:%S.%fZ')
+            stop_time = datetime.strptime(epg_stop, '%Y-%m-%dT%H:%M:%S.%fZ')
+            start_formatted = start_time.strftime('%Y%m%d%H%M%S')
+            stop_formatted = stop_time.strftime('%Y%m%d%H%M%S')
+        except:
+            start_formatted = ''
+            stop_formatted = ''
+        
+        # Process title and description
+        title = epg_title.replace('&', '&amp;') if epg_title else ''
+        description = name if name else ''
+        if epg_description:
+            if description:
+                description += ' - '
+            description += epg_description.replace('&', '&amp;')
+        
+        # Add program to XML
+        if channel_id and start_formatted and stop_formatted:
+            programs.append({
+                'channel_id': channel_id,
+                'start': start_formatted,
+                'stop': stop_formatted,
+                'title': title,
+                'description': description
+            })
+    
+    # Add to M3U playlist
+    m3u_lines.append(f'#EXTINF:-1 tvg-id="{channel_id}" tvg-chno="{channel_number}" tvg-logo="{thumbnail_url}",{name}')
+    m3u_lines.append(stream_url)
+    
+    # Add channel to XML
+    if channel_id:
+        channels.append({
+            'id': channel_id,
+            'name': name,
+            'thumbnail': thumbnail_url
+        })
+
+# Generate M3U file
+with open('m3u/fmplus.m3u', 'w', encoding='utf-8') as f:
+    f.write('\n'.join(m3u_lines))
+
+# Generate XMLTV file
 tv = ET.Element('tv')
 tv.set('source-info-url', 'https://bitcentral.com')
 tv.set('source-info-name', 'Bitcentral, Inc.')
 tv.set('generator-info-name', '')
 
-# Process each channel
-for channel_data in data:
-    # Extract data with fallbacks
-    epg_entries = channel_data.get('epg', {}).get('entries', [{}])
-    epg_entry = epg_entries[0] if epg_entries else {}
+# Add channels
+for channel in channels:
+    channel_elem = ET.SubElement(tv, 'channel')
+    channel_elem.set('id', channel['id'])
     
-    epg_description = epg_entry.get('description')
-    epg_start = epg_entry.get('start')
-    epg_stop = epg_entry.get('stop')
-    epg_title = epg_entry.get('title')
+    display_name = ET.SubElement(channel_elem, 'display-name')
+    display_name.text = channel['name'].replace('&', '&amp;') if channel['name'] else ''
     
-    thumbnails = channel_data.get('thumbnails', {}).get('light')
-    channel_id = channel_data.get('_id')
-    name = channel_data.get('name')
-    channel_number = channel_data.get('siteOptions', {}).get('channelNumber')
-    stream_url = channel_data.get('url')
-    
-    # Process data
-    description = name
-    if epg_description:
-        description = epg_description.replace('&', '&amp;')
-    
-    start_time = ''
-    stop_time = ''
-    if epg_start:
-        start_time = datetime.fromisoformat(epg_start.replace('Z', '+00:00')).strftime('%Y%m%d%H%M%S')
-    if epg_stop:
-        stop_time = datetime.fromisoformat(epg_stop.replace('Z', '+00:00')).strftime('%Y%m%d%H%M%S')
-    
-    title = ''
-    if epg_title:
-        title = epg_title.replace('&', '&amp;')
-    
-    # Process stream URL
-    stream = stream_url.replace('[', '%5B').replace(']', '%5D') if stream_url else ''
-    
-    # Add to M3U playlist
-    m3u_lines.append(f'#EXTINF:-1 tvg-id="{channel_id}" tvg-chno="{channel_number}" tvg-logo="{thumbnails}",{name}')
-    m3u_lines.append(stream)
-    
-    # Create XML channel element
-    channel = ET.SubElement(tv, 'channel')
-    channel.set('id', channel_id)
-    
-    display_name = ET.SubElement(channel, 'display-name')
-    display_name.text = name.replace('&', '&amp;') if name else ''
-    
-    if thumbnails:
-        icon = ET.SubElement(channel, 'icon')
-        icon.set('src', thumbnails)
-    
-    # Create XML programme element if we have EPG data
-    if start_time and stop_time and title:
-        programme = ET.SubElement(tv, 'programme')
-        programme.set('start', f'{start_time} +0000')
-        programme.set('stop', f'{stop_time} +0000')
-        programme.set('channel', channel_id)
-        
-        title_elem = ET.SubElement(programme, 'title')
-        title_elem.text = title
-        
-        desc_elem = ET.SubElement(programme, 'desc')
-        desc_elem.text = description if description else ''
-    
-    # Store for debugging/other uses
-    channels.append({
-        'id': channel_id,
-        'name': name,
-        'number': channel_number,
-        'logo': thumbnails,
-        'stream': stream
-    })
-    
-    if start_time and stop_time and title:
-        programmes.append({
-            'channel_id': channel_id,
-            'start': start_time,
-            'stop': stop_time,
-            'title': title,
-            'description': description
-        })
+    if channel['thumbnail']:
+        icon = ET.SubElement(channel_elem, 'icon')
+        icon.set('src', channel['thumbnail'])
 
-# Add M3U header
-m3u_header = ['#EXTM3U url-tvg="https://arkina1234.github.io/iptv-streams/epg/fmplus.xml"']
-m3u_content = m3u_header + m3u_lines
+# Add programs
+for program in programs:
+    programme = ET.SubElement(tv, 'programme')
+    programme.set('start', f"{program['start']} +0000")
+    programme.set('stop', f"{program['stop']} +0000")
+    programme.set('channel', program['channel_id'])
+    
+    title = ET.SubElement(programme, 'title')
+    title.text = program['title']
+    
+    desc = ET.SubElement(programme, 'desc')
+    desc.text = program['description']
 
-# Save M3U file
-with open('m3u/fmplus.m3u', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(m3u_content))
+# Format XML nicely
+xml_str = ET.tostring(tv, encoding='utf-8')
+parsed = minidom.parseString(xml_str)
+pretty_xml = parsed.toprettyxml(indent='  ')
 
-# Save XML file with proper formatting
-xml_str = ET.tostring(tv, encoding='unicode', method='xml')
-
-# Pretty print the XML
-xml_pretty = minidom.parseString(xml_str).toprettyxml(indent="  ")
-
-# Remove the XML declaration from minidom and add our custom one
-xml_pretty = xml_pretty.replace('<?xml version="1.0" ?>', '<?xml version="1.0" encoding="UTF-8" ?>\n<!DOCTYPE tv SYSTEM "xmltv.dtd">')
-
+# Save XML file
 with open('epg/fmplus.xml', 'w', encoding='utf-8') as f:
-    f.write(xml_pretty)
+    f.write(pretty_xml)
 
-print(f"Processed {len(channels)} channels")
-print(f"Generated {len(programmes)} programme entries")
-print("Files saved: m3u/fmplus.m3u and epg/fmplus.xml")
+print(f"Generated {len(channels)} channels")
+print("Files created:")
+print("- m3u/fmplus.m3u")
+print("- epg/fmplus.xml")
